478fa60050fda300ecb985836c52f09c08a782a4
laizy <laizhichao@onchain.com>
2018-10-23 18:49:37 +0800

    implement executor

--- vm/neovm/func_pushdata_test.go
+++ vm/neovm/func_pushdata_test.go
@@ -30,7 +30,7 @@ func TestGetPushData(t *testing.T) {
 	var e ExecutionEngine
 	var iRet int8
 	var ret []byte
-	e.Context = NewExecutionContext(&e, nil)
+	e.Context = NewExecutionContext( nil)
 
 	e.OpCode = PUSH0
 	iRet, ok := getPushData(&e).(int8)
