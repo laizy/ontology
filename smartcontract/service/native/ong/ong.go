01eae5bbf25b5baff4d8ccf6a8307959d48e08c7
lucas7788 <sishsh@163.com>
2018-12-20 11:07:48 +0800

    remove error import

--- smartcontract/service/native/ong/ong.go
+++ smartcontract/service/native/ong/ong.go
@@ -21,11 +21,9 @@ package ong
 import (
 	"math/big"
 
-	"bytes"
 	"fmt"
 	"github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/common/constants"
-	"github.com/ontio/ontology/common/serialization"
 	"github.com/ontio/ontology/errors"
 	"github.com/ontio/ontology/smartcontract/service/native"
 	"github.com/ontio/ontology/smartcontract/service/native/ont"
