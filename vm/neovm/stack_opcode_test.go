44318763622e61342c123361278d12f990f34a47
laizy <laizhichao@onchain.com>
2018-12-24 21:47:14 +0800

    add arithmetic unit test

--- vm/neovm/stack_opcode_test.go
+++ vm/neovm/stack_opcode_test.go
@@ -28,3 +28,36 @@ func checkStackOpCode(t *testing.T, code OpCode, origin, expected []int) {
 func TestStackOpCode(t *testing.T) {
 	checkStackOpCode(t, SWAP, []int{1, 2}, []int{2, 1})
 }
+
+func TestArithmetic(t *testing.T) {
+	checkStackOpCode(t, ADD, []int{1, 2}, []int{3})
+	checkStackOpCode(t, SUB, []int{1, 2}, []int{-1})
+
+	checkStackOpCode(t, MUL, []int{3, 2}, []int{6})
+
+	checkStackOpCode(t, DIV, []int{3, 2}, []int{1})
+	checkStackOpCode(t, DIV, []int{103, 2}, []int{51})
+
+	checkStackOpCode(t, MAX, []int{3, 2}, []int{3})
+	checkStackOpCode(t, MAX, []int{-3, 2}, []int{2})
+
+	checkStackOpCode(t, MIN, []int{3, 2}, []int{2})
+	checkStackOpCode(t, MIN, []int{-3, 2}, []int{-3})
+
+	checkStackOpCode(t, SIGN, []int{3}, []int{1})
+	checkStackOpCode(t, SIGN, []int{-3}, []int{-1})
+	checkStackOpCode(t, SIGN, []int{0}, []int{0})
+
+	checkStackOpCode(t, INC, []int{-10}, []int{-9})
+	checkStackOpCode(t, DEC, []int{-10}, []int{-11})
+	checkStackOpCode(t, NEGATE, []int{-10}, []int{10})
+	checkStackOpCode(t, ABS, []int{-10}, []int{10})
+
+	checkStackOpCode(t, NOT, []int{1}, []int{0})
+	checkStackOpCode(t, NOT, []int{0}, []int{1})
+
+	checkStackOpCode(t, NZ, []int{0}, []int{0})
+	checkStackOpCode(t, NZ, []int{-10}, []int{1})
+	checkStackOpCode(t, NZ, []int{10}, []int{1})
+
+}
