8f9c77872e375931430714e49b51bde96bc15cdd
lucas7788 <sishsh@163.com>
2019-01-08 11:25:14 +0800

    fix DCALL bug

--- vm/neovm/executor.go
+++ vm/neovm/executor.go
@@ -179,6 +179,18 @@ func (self *Executor) ExecuteOp(opcode OpCode, context *ExecutionContext) (VMSta
 		if needJmp {
 			context.SetInstructionPointer(int64(offset))
 		}
+	case DCALL:
+		caller := context.Clone()
+		caller.SetInstructionPointer(int64(caller.GetInstructionPointer()))
+		self.PushContext(caller)
+		target, err := self.EvalStack.PopAsInt64()
+		if err != nil {
+			return FAULT, err
+		}
+		if target < 0 || target >= int64(len(self.Context.Code)) {
+			return FAULT, errors.ERR_DCALL_OFFSET_ERROR
+		}
+		self.Context.SetInstructionPointer(target)
 	case RET:
 		self.Context, _ = self.PopContext()
 
@@ -188,18 +200,6 @@ func (self *Executor) ExecuteOp(opcode OpCode, context *ExecutionContext) (VMSta
 				SYSCALL  OpCode = 0x68
 		*/
 		// Stack
-	case DCALL:
-		caller := context.Clone()
-		caller.SetInstructionPointer(int64(caller.GetInstructionPointer()))
-		self.PushContext(caller)
-		target, err := self.EvalStack.PopAsInt64()
-		if err != nil {
-			return FAULT, err
-		}
-		if target < 0 || int(target) >= len(self.Context.Code) {
-			return FAULT, errors.ERR_DCALL_OFFSET_ERROR
-		}
-		self.Context.SetInstructionPointer(target)
 	case DUPFROMALTSTACK:
 		val, err := self.AltStack.Peek(0)
 		if err != nil {
