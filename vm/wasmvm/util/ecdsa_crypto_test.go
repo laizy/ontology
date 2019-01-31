ee32eef87728c3e75d3f698961acc899f3c9ee0c
NashMiao <18191964+NashMiao@users.noreply.github.com>
2019-01-31 15:23:16 +0800

    rm wasm tests

--- vm/wasmvm/util/ecdsa_crypto_test.go
+++ vm/wasmvm/util/ecdsa_crypto_test.go
@@ -16,38 +16,38 @@
  * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
  */
 package util
-
-import (
-	"fmt"
-	"testing"
-)
-
-func TestECDsaCrypto_Hash160(t *testing.T) {
-	ecdsa := &ECDsaCrypto{}
-	b := []byte("test string")
-	res := ecdsa.Hash160(b)
-	if len(res) != 20 {
-		t.Error("TestECDsaCrypto_Hash160 length is not 20")
-	}
-	b = []byte(nil)
-	res = ecdsa.Hash160(b)
-	if len(res) != 20 {
-		t.Error("TestECDsaCrypto_Hash160 length is not 20")
-	}
-
-}
-
-func TestECDsaCrypto_Hash256(t *testing.T) {
-	ecdsa := &ECDsaCrypto{}
-	b := []byte("test string")
-	res := ecdsa.Hash256(b)
-	fmt.Println(res)
-	if len(res) != 32 {
-		t.Error("TestECDsaCrypto_Hash160 length is not 20")
-	}
-	b = []byte(nil)
-	res = ecdsa.Hash256(b)
-	if len(res) != 32 {
-		t.Error("TestECDsaCrypto_Hash160 length is not 20")
-	}
-}
+//
+//import (
+//	"fmt"
+//	"testing"
+//)
+//
+//func TestECDsaCrypto_Hash160(t *testing.T) {
+//	ecdsa := &ECDsaCrypto{}
+//	b := []byte("test string")
+//	res := ecdsa.Hash160(b)
+//	if len(res) != 20 {
+//		t.Error("TestECDsaCrypto_Hash160 length is not 20")
+//	}
+//	b = []byte(nil)
+//	res = ecdsa.Hash160(b)
+//	if len(res) != 20 {
+//		t.Error("TestECDsaCrypto_Hash160 length is not 20")
+//	}
+//
+//}
+//
+//func TestECDsaCrypto_Hash256(t *testing.T) {
+//	ecdsa := &ECDsaCrypto{}
+//	b := []byte("test string")
+//	res := ecdsa.Hash256(b)
+//	fmt.Println(res)
+//	if len(res) != 32 {
+//		t.Error("TestECDsaCrypto_Hash160 length is not 20")
+//	}
+//	b = []byte(nil)
+//	res = ecdsa.Hash256(b)
+//	if len(res) != 32 {
+//		t.Error("TestECDsaCrypto_Hash160 length is not 20")
+//	}
+//}
